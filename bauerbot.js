2de1f4ddd0a1ab905f2dec54d1bade76f7a11423
cdub-m <not.cdub@gmail.com>
2020-01-15 08:19:47 -0500

    update

--- bauerbot.js
+++ bauerbot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = 'b!';
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
