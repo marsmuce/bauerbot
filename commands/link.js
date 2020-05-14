d32977a494bda1ba785694e0af4b624629eadafd
Christian Muce <lowercase.txt@gmail.com>
2020-05-14 12:47:34 -0400

    Update link.js

--- commands/link.js
+++ commands/link.js
@@ -13,26 +13,35 @@ module.exports = {
 							name: "Downloads:",
 							value: "[Microsoft Word](https://cdn.discordapp.com/attachments/583104406317301773/583291743223545886/The_War_for_New_Detroit.docx)\n[Plain Text](https://cdn.discordapp.com/attachments/583104406317301773/583291314792038411/The_War_for_New_Detroit.txt)\n[PDF Document](https://cdn.discordapp.com/attachments/583104406317301773/583290543606464523/The_War_for_New_Detroit.pdf)\n[EPUB Publication](https://cdn.discordapp.com/attachments/583104406317301773/583291114102980618/The_War_for_New_Detroit.epub)",
 						}],
+            footer: {
+					    text: '© Christopher Nokes'
+				    }
 					}});
 					break;
 				case 'hark':
 					message.channel.send({embed: {
 						title: "The Matthew Hark Story",
 						url: "https://docs.google.com/document/d/1ZPmLpFfGh7Dd9jzMhqVg4Gzfmr-G68NBscTSZ3t-kbg/",
-						/*fields: [{
+						fields: [{
 							name: "Downloads:",
-							value: "[Microsoft Word](https://cdn.discordapp.com/attachments/583104406317301773/583291743223545886/The_War_for_New_Detroit.docx)\n[Plain Text](https://cdn.discordapp.com/attachments/583104406317301773/583291314792038411/The_War_for_New_Detroit.txt)\n[PDF Document](https://cdn.discordapp.com/attachments/583104406317301773/583290543606464523/The_War_for_New_Detroit.pdf)\n[EPUB Publication](https://cdn.discordapp.com/attachments/583104406317301773/583291114102980618/The_War_for_New_Detroit.epub)",
-						}],*/
+							value: "[Microsoft Word]()\n[Plain Text]()\n[PDF Document]()\n[EPUB Publication]()",
+						}],
+            footer: {
+					    text: '© BOTTOM TEXT'
+				    }
 					}});
 					break;
 				case 'waaagh':
 					message.channel.send({embed: {
 						title: "‘Da WAAAGH! For New Detroit",
 						url: "https://docs.google.com/document/d/1VP4hwBqrMxq_bLUjwBn6FKyCGKfIB27LIc9M6zmH_OM/",
-						/*fields: [{
+						fields: [{
 							name: "Downloads:",
-							value: "[Microsoft Word](https://cdn.discordapp.com/attachments/583104406317301773/583291743223545886/The_War_for_New_Detroit.docx)\n[Plain Text](https://cdn.discordapp.com/attachments/583104406317301773/583291314792038411/The_War_for_New_Detroit.txt)\n[PDF Document](https://cdn.discordapp.com/attachments/583104406317301773/583290543606464523/The_War_for_New_Detroit.pdf)\n[EPUB Publication](https://cdn.discordapp.com/attachments/583104406317301773/583291114102980618/The_War_for_New_Detroit.epub)",
-						}],*/
+							value: "[Microsoft Word]()\n[Plain Text]()\n[PDF Document]()\n[EPUB Publication]()",
+						}],
+            footer: {
+					    text: '© BOTTOM TEXT'
+				    }
 					}});
 					break;
 				default:
