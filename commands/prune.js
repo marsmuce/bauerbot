436a8278eabc309b331c2f7ada68e7a41ca4dbb3
Christian Muce <lowercase.txt@gmail.com>
2020-05-14 10:46:22 -0400

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -1,30 +1,17 @@
 module.exports = {
-	name: 'prune',
-	description: 'bulk delete messages',
-	admin: true,
-	execute(message, args) {
-		let p = parseInt(args[0]) + 1;
-		let dupe = parseInt(args[1]);
-
-		if (isNaN(p)) {
-			return message.reply('that doesn\'t seem to be a valid number.');
-		} else if (p <= 1 || p > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
-		}
-		if (isNan(dupe)) {
-			if (isNaN(p)) {
-				return message.reply('that doesn\'t seem to be a valid number.');
-			} else if (p <= 1 || p > 100) {
-				return message.reply('you need to input a number between 1 and 99.');
-			}
-			message.channel.bulkDelete(p, true).catch(err => {
-			console.error(err); message.channel.send(err);});
-		} else if (dupe => 1) {
-			while (dupe > 0) {
-				message.channel.bulkDelete(p, true).catch(err => {
-				console.error(err); message.channel.send(err);});
-				dupe = dupe - 1;
-			}
-		}
-	},
+  name: 'prune',
+  description: 'bulk delete messages',
+  admin: true,
+  execute(message, args) {
+    let p = parseInt(args[0]) + 1;
+    if (isNaN(p) || p <= 1) {
+      return message.reply('that doesn\'t seem to be a valid number.');
+    } else if (p > 99) {
+      let px = p - 99;
+      message.channel.bulkDelete(99, true);
+      message.channel.bulkDelete(px, true);
+    } else {
+      message.channel.bulkDelete(p, true);
+    }
+  },
 };
