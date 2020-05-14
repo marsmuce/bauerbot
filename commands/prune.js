6cc6b933075a61f128fa61ae1bb599499d1897fb
Christian Muce <lowercase.txt@gmail.com>
2020-05-14 12:05:20 -0400

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -5,7 +5,7 @@ module.exports = {
   execute(message, args) {
     let p = parseInt(args[0]) + 1;
     if (isNaN(p) || p <= 1 || p >= 100 ) {
-      return message.reply('that doesn\'t seem to be a valid number between 0 and 100.');
+      return message.reply('that doesn\'t seem to be a valid integer between 0 and 100.');
     } else {
       message.channel.bulkDelete(p);
     }
