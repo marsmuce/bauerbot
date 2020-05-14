5d7aa09b4211454dfb90d39b49151d8ca8f1e4f6
Christian Muce <lowercase.txt@gmail.com>
2020-05-14 12:32:33 -0400

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'clone and delete channel',
   admin: true,
   execute(message, args) {
-    if (args[0] === 'for' && args[1] === 'the' && args[2] === 'blood' && args[3] === 'god!' &&) {
+    if (args[0] === 'for' && args[1] === 'the' && args[2] === 'blood' && args[3] === 'god!') {
       message.channel.clone();
       message.channel.delete();
     }
