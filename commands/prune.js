a25491194e4b354d3d60f19f9ef9f8459df71c7a
Christian Muce <lowercase.txt@gmail.com>
2020-05-14 11:45:39 -0400

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -7,11 +7,7 @@ module.exports = {
     if (isNaN(p) || p <= 1) {
       return message.reply('that doesn\'t seem to be a valid number.');
     } else if (p > 99) { 
-      let px = p - 99; if (px > 99) { let py = px - 99; if (py > 99) { let pz = py - 99; if (pz > 99) { let pz = 99; } } else { py = py - 1; let pz = 1 } } else { px = px - 1 - 1; let py = 1; let pz = py; }
-      message.channel.bulkDelete(99, true);
-      message.channel.bulkDelete(px, true);
-      message.channel.bulkDelete(py, true);
-      message.channel.bulkDelete(pz, true);
+      let px = p - 99; if (px > 99) { let py = px - 99; if (py > 99) { let pz = py - 99; if (pz > 99) { let pz = 99; } message.channel.bulkDelete(99, true); message.channel.bulkDelete(px, true); message.channel.bulkDelete(py, true); message.channel.bulkDelete(pz, true); } message.channel.bulkDelete(99, true); message.channel.bulkDelete(px, true); message.channel.bulkDelete(py, true); } message.channel.bulkDelete(99, true); message.channel.bulkDelete(px, true);
     } else {
       message.channel.bulkDelete(p, true);
     }
