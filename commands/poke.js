f14b77f1bc4134598d054e3dbdf9911a8c82dd9f
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2020-05-14 10:17:59 -0400

    Update poke.js

--- commands/poke.js
+++ commands/poke.js
@@ -5,7 +5,7 @@ module.exports = {
         let taggedUser = message.mentions.users.first();
         let author = message.guild.members.get(message.author.id).displayName.toString();
 
-        if(typeof args[0] != 'undefined' && args[0] != null && (args[0]<=2560 || message.author.id === '296712085532966915')) {
+        if(typeof args[0] != 'undefined' && args[0] != null && (args[0]<=1000 || message.author.id === '296712085532966915')) {
             for (var i = 0; i < args[0]; i++) { 
                 message.channel.send(`\`@${author}\` poked ${taggedUser} with a stick. (${i+1}/${args[0]})`);
             } 
