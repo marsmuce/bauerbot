e6ad1053cb65ba984b36755ccb6e0385bb8ce2d8
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2020-05-14 10:23:23 -0400

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -7,9 +7,9 @@ module.exports = {
 
 		if (isNaN(p)) {
 			return message.reply('that doesn\'t seem to be a valid number.');
-		} else if (p <= 1 || p > 10000) {
-			return message.reply('you need to input a number between 1 and 99.');
-		}
+//		} else if (p <= 1 || p > 100) {
+//			return message.reply('you need to input a number between 1 and 99.');
+//		}
 
 		message.channel.bulkDelete(p, true).catch(err => {
 		console.error(err); message.channel.send(err);});
