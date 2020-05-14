c937780cfc05a0194fa3a6b0505bac3aad1afb9c
Christian Muce <lowercase.txt@gmail.com>
2020-05-14 12:04:18 -0400

    revert prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -4,10 +4,8 @@ module.exports = {
   admin: true,
   execute(message, args) {
     let p = parseInt(args[0]) + 1;
-    if (isNaN(p) || p <= 1) {
-      return message.reply('that doesn\'t seem to be a valid number.');
-    } else if (p > 99) { 
-      let px = p - 99; if (px > 99) { let py = px - 99; if (py > 99) { let pz = py - 99; if (pz > 99) { let pz = 99; } message.channel.bulkDelete(99); message.channel.bulkDelete(px); message.channel.bulkDelete(py); message.channel.bulkDelete(pz); } message.channel.bulkDelete(99); message.channel.bulkDelete(px); message.channel.bulkDelete(py); } message.channel.bulkDelete(99); message.channel.bulkDelete(px);
+    if (isNaN(p) || p <= 1 || p >= 100 ) {
+      return message.reply('that doesn\'t seem to be a valid number between 0 and 100.');
     } else {
       message.channel.bulkDelete(p);
     }
