ad99313f47bc08a01730d0988bf4f99fa2073753
Christian Muce <lowercase.txt@gmail.com>
2020-05-16 17:56:53 -0400

    Delete coinflip.js

--- commands/coinflip.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'coinflip',
-	description: 'flip a coin.',
-	execute(message) {
-		message.channel.send((Math.floor(Math.random() * 2) == 0) ? 'Heads' : 'Tails');
-	},
-};
