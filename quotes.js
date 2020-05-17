480170c5e2750ecf4abc01021bece8ac3bbf87ef
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2020-05-17 19:58:38 -0400

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -11,41 +11,7 @@ module.exports = {
       'Where the fuck is my son?',
       'Bauer 2 when, @Imperator Nokov#8212?',
       'Imagine it like a jar of marbles. Each one of them- with different sizes, shapes, makeups- all have some very simple things in common, correct? They all border each other, they are all affected by the same gravitational pull and laws of physics, et cetera. Now, consider the idea that every single one of those marbles is representative of a universe. Not necessarily ours, but a universe. Now imagine that, instead of being contained to a measly jar, the space of marbles is an infinite sea. So, an infinite space of universes. And, given it’s infinite size, there must be an infinite number of universes that are equivalent to our own, correct? This is not Aetherspace. This is reality. The space between the marbles is a void in which there is nothing. Aetherspace is- essentially- that entire construct with the walls of each universe torn down. It is also exponentially smaller and representative of our space, allowing for teleportation and whatnot, but this is insignificant in the grand scheme of things. It is, in simplicity, that we can reach out to other universes. That everything exists. Things far greater than our own, ripe for the taking. I’m not suggesting piracy- I pay people to do that for me- but imagine if we found something great enough out there that could make us masters of our own reality. Or what if we found another universe in which we could be it’s masters? But I do not wish for the latter. I am not a coward; I will not abandon this world to find my utopia. I shall make it myself.',
-      'Cassius: Here we are at last, Mr Bauer. Do you want to play a quick game of uno and I can record it and put it in tomorrow's Aeternum Post?
- 
-Bauer: I don't have uno, you sociopath
-
-Cassius: Everyone has Uno dipshit, it came free with The War For New Detroit
-
-Bauer: I didn't get it, I have the oldest copy known to man
-
-Cassius: No you don't, I got mine day one you fucking War Dog
-
-Bauer: Well mine didn't have i-
-
-Cassius: YOU HAVE UNO YOU ANNOYING LITTLE SHIT!
-
-Bauer: I DON'T HAVE IT YOU-
-
-Cassius: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!
-
-Bauer: I DON'T HAVE FUCKING UNO YOU MONSTROSITY!
-
-Cassius: GO TO CONSTANTI ACADEMY AND YOU'LL BE ABLE TO GET IT FOR FREE YOU DENSE PILE OF FISH FOOD!
-
-Bauer: I DON'T HAVE CHAPTER 2 HOPLESSNESS, I DON'T HAVE CHAPTER 3 DARKNESS, I DON'T HAVE FUCKING CHAPTER 4 CARLA!
-
-Cassius: IT'S A FUCKING CARD GAME, THEY DON'T EVEN CHARGE PEOPLE FOR IT!
-
-Bauer: I DON'T HAVE CHAPTER 6 RETREAT, CHAPTER 7 HUNTED, CHAPTER 8 CALLING, CHAPTER 9 REPARATIONS, CHAPTER 10 MEMORIES, OR CHAPTER 11 BLINDED!
-
-Cassius: YOU DON'T KNOW A GOD DAMN THING MR BAUER, IT'S FUCKING UNO! IT'S FREE!
-
-Bauer: I DON'T HAVE UNO!
-
-Cassius: IT'S FREE AS FUCK!
-
-Carla: Is this going on Youtube?' ] },
+      'Cassius: Here we are at last, Mr Bauer. Do you want to play a quick game of uno and I can record it and put it in tomorrows Aeternum Post? Bauer: I dont have uno, you sociopath Cassius: Everyone has Uno dipshit, it came free with The War For New Detroit Bauer: I didnt get it, I have the oldest copy known to man Cassius: No you dont, I got mine day one you fucking War Dog Bauer: Well mine didnt have i- Cassius: YOU HAVE UNO YOU ANNOYING LITTLE SHIT! Bauer: I DONT HAVE IT YOU- Cassius: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!! Bauer: I DONT HAVE FUCKING UNO YOU MONSTROSITY! Cassius: GO TO CONSTANTI ACADEMY AND YOULL BE ABLE TO GET IT FOR FREE YOU DENSE PILE OF FISH FOOD! Bauer: I DONT HAVE CHAPTER 2 HOPLESSNESS, I DONT HAVE CHAPTER 3 DARKNESS, I DONT HAVE FUCKING CHAPTER 4 CARLA! Cassius: ITS A FUCKING CARD GAME, THEY DONT EVEN CHARGE PEOPLE FOR IT! Bauer: I DONT HAVE CHAPTER 6 RETREAT, CHAPTER 7 HUNTED, CHAPTER 8 CALLING, CHAPTER 9 REPARATIONS, CHAPTER 10 MEMORIES, OR CHAPTER 11 BLINDED! Cassius: YOU DONT KNOW A GOD DAMN THING MR BAUER, ITS FUCKING UNO! ITS FREE! Bauer: I DONT HAVE UNO! Cassius: ITS FREE AS FUCK! Carla: Is this going on Youtube?' ] },
 
     { "name":"classic", "quotes":[ 
 
