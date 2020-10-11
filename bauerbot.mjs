c3a9fef832b3eb7cdf92c08ac5ff9cffa9074ac1
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2020-10-11 17:26:07 -0400

    Update and rename bauerbot.js to bauerbot.mjs
    fuck you

--- bauerbot.mjs
+++ bauerbot.mjs
@@ -1,5 +1,7 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+import {
+  Discord,
+  fs
+} from './imports.mjs'
 const client = new Discord.Client();
 
 const prefix = 'b!';
@@ -18,32 +20,22 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  	 const args = message.content.slice(prefix.length).split(/ +/),
+	 commandName = args.shift().toLowerCase(),
+	 command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
 	if (!command) return;
-
 	
 	if(message.guild !== null) { 
 		const admin = message.member.hasPermission("ADMINISTRATOR");
-		if (command.admin && !message.member.hasPermission("ADMINISTRATOR")) {
-			return message.reply('Permissions required to execute this command');
-		}
+		if (command.admin && !message.member.hasPermission("ADMINISTRATOR")) return message.reply('Permissions required to execute this command');
 	}
 
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}	
+	if (command.guildOnly && message.channel.type !== 'text') return message.reply('I can\'t execute that command inside DMs!');
 
 	if (command.args && !args.length) {
 		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
+		if (command.usage) reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
 		return message.channel.send(reply);
 	}
 
@@ -51,32 +43,31 @@ client.on('message', message => {
 		command.execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply('error');
+		message.reply({embed: {
+			setTitle: 'Error',
+			setDescription: "```js\n" + JSON.stringify(error) + "```"
+		}});
 	}
 });
 
 client.on('message', message => {
     client.responses = new Discord.Collection();
 	const responseFiles = fs.readdirSync('./responses').filter(file => file.endsWith('.js'));
-	for (const file of responseFiles) {
-		const response = require(`./responses/${file}`);
-		client.responses.set(response.name, response);
-	}
+	responseFiles.forEach(file => {
+            const response = require(`./responses/${file}`);
+	    client.responses.set(response.name, response);
+	})
 	const responseName = message.content.toLowerCase();
-	const response = client.responses.get(responseName)
-		|| client.responses.find(re => re.aliases && re.aliases.includes(responseName));
+	const response = client.responses.get(responseName) || client.responses.find(re => re.aliases && re.aliases.includes(responseName));
 
-	if (!response) return;
-	response.execute(message);
+	return reponse ? response.execute(message) : null;
 
 });
 
 client.on('message', message => {
 	const echo = require('./responses/echo.json');
 	if (!echo) return;
-    if( echo.some(word => message.content.toLowerCase() == word) && !message.author.bot) {
-        message.channel.send(message.content);
-    }
+        if (echo.some(word => message.content.toLowerCase() == word) && !message.author.bot) return  message.channel.send(message.content);
 });
 
 client.login(process.env.TOKEN);
