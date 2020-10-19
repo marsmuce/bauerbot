eec9d032b0d286f6efd31e0abea7e7f44c222c1f
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2020-10-19 17:26:19 -0400

    Delete n.js

--- n.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'n',
-	description: 'Yeah, I've got time.',
-	execute(message, args) {
-		message.channel.send('https://cdn.discordapp.com/attachments/379067857893589004/767568589997211658/1018201903_HDR.jpg');
-	},
-};
