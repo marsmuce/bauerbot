4b55c34e0dc6c3eb0f735953d10e74d4c520d8f4
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2020-10-19 17:26:30 -0400

    Create n.js

---
+++ commands/n.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'n',
+	description: 'Yeah, I've got time.',
+	execute(message, args) {
+		message.channel.send('https://cdn.discordapp.com/attachments/379067857893589004/767568589997211658/1018201903_HDR.jpg');
+	},
+};
