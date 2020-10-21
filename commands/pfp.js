ac74791d8d284e43d0cd3d7ff300d24e94625ad9
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2020-10-21 09:27:40 -0400

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -2,9 +2,7 @@ module.exports = {
 	name: 'pfp',
 	description: 'get user pfp',
 	execute(message) {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
-		}
+		if (!message.mentions) return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
 
 		const pfp = message.mentions.users.map(user => {
 			return `${user.username}'s avatar: ${user.displayAvatarURL}`;
