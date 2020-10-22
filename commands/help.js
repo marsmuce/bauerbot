4d8231b870bea03cbb09ae359934ce337ade7bb7
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2020-10-22 10:33:51 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'help',
 	description: 'fucker',
 	execute(message, args) {
-		message.channel.send('**Commands:** \n`b!auer [null, hark, waaagh]` - Sends The War for New Detroid \n`b!poke [number] [ping]` - Pings a user a number of times.\n`b!quote [remastered, universe, classic]` - Sends a quote from the legendary Adrian Bauer.\n`b!prune [number] [admin only]` - Removes a number of messages.\n**GitHub:** <https://github.com/lowercasetxt/bauerbot>\n**Update Version:** 1.0.3');
+		message.channel.send('**Commands:** \n`b!auer [null, hark, waaagh]` - Sends The War for New Detroid \n`b!poke [number] [ping]` - Pings a user a number of times.\n`b!quote [remastered, universe, classic]` - Sends a quote from the legendary Adrian Bauer.\n`b!prune [number] [admin only]` - Removes a number of messages.\n**GitHub:** <https://github.com/lowercasetxt/bauerbot>\n**Update Version:** 1.0.4');
 	},
 };
