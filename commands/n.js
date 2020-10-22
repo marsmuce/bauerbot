cced10d7d5d230f7bb5173a361202e033e85c70c
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2020-10-22 10:42:51 -0400

    Update n.js

--- commands/n.js
+++ commands/n.js
@@ -4,9 +4,10 @@ module.exports = {
 	execute(message, args) {
 		var rand_num = 0;
 		message.channel.send(args[0]);
-		if(typeof args[0] != 'undefined' && args[0] != null && args[0] >= 0 && args[0] <= 21) rand_num = args[0];
-		message.channel.send("Rand num is " + rand_num);
-		else if(typeof args[0] == 'undefined' || args[0] == null || args[0] < 0 || args[0] > 21) rand_num = Math.floor(Math.random() * 21);
+		if(typeof args[0] != 'undefined' && args[0] != null && args[0] >= 0 && args[0] <= 21) {
+			rand_num = args[0];
+			message.channel.send("Rand num is " + rand_num);
+		} else if(typeof args[0] == 'undefined' || args[0] == null || args[0] < 0 || args[0] > 21) rand_num = Math.floor(Math.random() * 21);
 		switch(rand_num) {
 			case 0:
 				message.channel.send('https://cdn.discordapp.com/attachments/379067857893589004/767568589997211658/1018201903_HDR.jpg');
