0b50f0b448c1e49738010778d0fff5b019570be4
cnokes14 <50048038+cnokes14@users.noreply.github.com>
2021-06-03 19:18:17 -0400

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -4,8 +4,8 @@ module.exports = {
   admin: true,
   execute(message, args) {
     let p = parseInt(args[0]) + 1;
-    if (isNaN(p) || p < 1 || p > 1001 ) {
-      return message.reply('That doesn\'t seem to be a valid integer between 1 and 1000, inclusive.');
+    if (isNaN(p) || p < 1 || p > 100 ) {
+      return message.reply('That doesn\'t seem to be a valid integer between 0 and 99, inclusive.');
     } else {
       message.channel.bulkDelete(p, true);
     }
